f820fc6ac51dbc0b80817e37a8d32b0157fd1429
Andras Nemeth <guni82hu@gmail.com>
2017-09-01 12:46:06 +0200

    card turning animation

--- app/src/main/java/hu/andras/cardsdemo/ui/main/CardBindingAdapter.java
+++ app/src/main/java/hu/andras/cardsdemo/ui/main/CardBindingAdapter.java
@@ -1,6 +1,8 @@
 package hu.andras.cardsdemo.ui.main;
 
+import android.animation.Animator;
 import android.databinding.BindingAdapter;
+import android.view.ViewPropertyAnimator;
 import android.widget.ImageView;
 
 import lombok.experimental.UtilityClass;
@@ -8,8 +10,34 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CardBindingAdapter {
 
+    public static final int ANIMATION_DURATION = 200;
+
     @BindingAdapter("imageResource")
-    public static void setImageResource(ImageView imageView, int resource){
-        imageView.setImageResource(resource);
+    public static void setImageResource(final ImageView imageView, final int resource){
+        ViewPropertyAnimator animator = imageView.animate().scaleX(0f).setDuration(ANIMATION_DURATION);
+        animator.setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                imageView.setImageResource(resource);
+                imageView.animate().scaleX(1f).setDuration(ANIMATION_DURATION).start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+        animator.start();
+
     }
 }
