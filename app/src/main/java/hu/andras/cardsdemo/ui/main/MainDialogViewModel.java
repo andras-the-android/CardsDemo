1ea6607047e1461b9227a0e45006d0b583387dee
Andras Nemeth <guni82hu@gmail.com>
2017-09-07 01:25:15 +0200

    main dialog skeleton fix build

--- app/src/main/java/hu/andras/cardsdemo/ui/main/MainDialogViewModel.java
+++ app/src/main/java/hu/andras/cardsdemo/ui/main/MainDialogViewModel.java
@@ -1,6 +1,26 @@
 package hu.andras.cardsdemo.ui.main;
 
-public class MainDialogViewModel {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class MainDialogViewModel extends BaseObservable {
+
+    public void onNewGameClick() {
+
+    }
+
+    public void onSaveNameClick() {
+
+    }
+
+    @Bindable
+    public String getName() {
+        return "";
+    }
+
+    public void setName(String name) {
+
+    }
 
 
 }
